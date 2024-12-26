06ba38bb336ac62a4066fef0d69c6e62db15d571
Raj kumar <rajkumar.rk0104@gmail.com>
2024-12-26 23:22:15 +0530

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -1,7 +1,8 @@
-<< insertion operator
+<< : insertion operator
 cout << "Hello, World!"  //means "insert the string Hello, World! into the output stream."
 
->>  extraction operator or stream extraction operator
+>> : extraction operator or stream extraction operator
 cin >> age; // Extracts input from the user and stores it in the variable 'age'
+
 
 
